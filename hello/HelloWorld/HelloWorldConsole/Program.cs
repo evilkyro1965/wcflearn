73fe652d30b8f8751791bc105ec582f0e3b30df7
Fahrur <evilkyro1965@yahoo.com>
2015-09-21 16:53:05 +0700

    initial

---
+++ hello/HelloWorld/HelloWorldConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldServiceClient client = new HelloWorldServiceClient();
+            Console.WriteLine(client.GetMessage("Fahrur"));
+        }
+    }
+}
